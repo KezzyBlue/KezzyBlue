78e1ad2561caed1521231d3c3b277d51cea06518
KezzyBlue <bluekezzy@gmail.com>
2026-07-13 14:23:10 +0700

    nothing new

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,11 @@
 function About()
 {
     return(
-        <> </>
+        <> 
+            <h1>
+                Is beeing developed
+            </h1>
+        </>
     );
 }
 export default About;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
 function Home()
 {
     return(
-        <> </>
+        <>
+            <h1>
+                Is beeing developed
+            </h1> 
+        </>
     );
 }
 export default Home;
